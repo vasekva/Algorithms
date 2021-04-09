758db0b7214fedee499400ef76845e79d6fbbb1f
Josephine Beregond <jberegon@un-l3.kzn.21-school.ru>
2021-04-10 00:33:03 +0300

    Переименован пакет coffeeMachine и сделан первый stage проекта "Cinema Room Manager"

---
+++ src/cinemaRoomManager/Cinema.java
@@ -0,0 +1,30 @@
+package cinemaRoomManager;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        System.out.println("Cinema:");
+        // цикл по строчкам
+        for (int i = 0; i < 8; i++) {
+            // цикл по столбцам
+            for (int j = 0; j < 9; j++) {
+                if (i == 0) {
+                    if (j == 0)
+                        System.out.print(" ");
+                    else
+                        System.out.print(j);
+                } else {
+                    if (j == 0)
+                        System.out.print(i);
+                    else
+                        System.out.print("S");
+                }
+                if (j + 1 != 9) {
+                    System.out.print(" ");
+                } else {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
