8e0cbfa354ad590b2c449be7439c909448b8db9b
vasekva <vahonin_vasek@mail.ru>
2021-04-10 13:51:37 +0300

    Отрефакторил функцию замены столбцов матрицы

--- src/multiDimensionalArray/SwapTheColumns.java
+++ src/multiDimensionalArray/SwapTheColumns.java
@@ -2,35 +2,53 @@ package multiDimensionalArray;
 
 import java.util.Scanner;
 
+/*
+* Дан двумерный массив (матрица) и два числа: i и j.
+* Необходимо поменять местами столбцы с индексами i и j в матрице.
+*
+* Входные данные содержат размеры матрицы n и m, не превышающие 100,
+* затем элементы матрицы, затем индексы i и j.
+*/
+
 public class SwapTheColumns {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int line = scanner.nextInt();
-        int column = scanner.nextInt();
-
-        int[][] array = new int[line][column];
-
-        for (int i = 0; i < line; i++) {
-            for (int j = 0; j < column; j++) {
-                array[i][j] = scanner.nextInt();
-            }
-        }
-        int firstColumn = scanner.nextInt();
-        int secondColumn = scanner.nextInt();
-
-        for (int i = 0; i < line; i++) {
-            int buf = array[i][firstColumn];
-            array[i][firstColumn] = array[i][secondColumn];
-            array[i][secondColumn] = buf;
+    private void swapAndPrintArr(int[][] array, int first, int second) {
+        // цикл замены
+        for (int i = 0; i < array.length; i++) {
+            int buf = array[i][first];
+            array[i][first] = array[i][second];
+            array[i][second] = buf;
 
         }
-
-        for (int i = 0; i < line; i++) {
-            for (int j = 0; j < column; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        SwapTheColumns  swap;
+        Scanner         scanner;
+        int             firstCol;
+        int             secondCol;
+        int[][]         array;
+        int             h;
+        int             w;
+
+        swap = new SwapTheColumns();
+        scanner = new Scanner(System.in);
+        h = scanner.nextInt();
+        w = scanner.nextInt();
+        array = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                array[i][j] = scanner.nextInt();
+            }
+        }
+        firstCol = scanner.nextInt();
+        secondCol = scanner.nextInt();
+        swap.swapAndPrintArr(array, firstCol, secondCol);
     }
 }
